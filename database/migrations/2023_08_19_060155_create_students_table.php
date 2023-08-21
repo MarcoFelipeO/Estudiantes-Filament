09058941231e2c8dd2bea3c2d80b92d2de44b1c1
Marko <mafeorellana09@gmail.com>
2023-08-21 09:28:51 -0400

    first commit

--- database/migrations/2023_08_19_060155_create_students_table.php
+++ database/migrations/2023_08_19_060155_create_students_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->date('date_of_birth');
             $table->timestamps();
         });
     }
